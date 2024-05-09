5ead1ec94f58e22a2fbfe68557e5361c28e1773e
AntrugosAyte <anderson@ayte.co>
2024-05-09 10:04:28 -0500

    [FEATURE] added styled on login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -25,15 +25,10 @@ export default function Login() {
       {/* <Nbar></Nbar> */}
       <div className="BackgroundLogin">
         <header className="LoginHeader">
-          <h1 className="title">Inicia sesión en tu cuenta</h1>
-          <div className="flex flex-row space-x-2 items-center justify-center">
-            <h5 className="text-center">¿Aún no tienes una cuenta?</h5>
-            <Link
-              className="font-bold text-sky-800 hover:text-blue-950"
-              to="/register"
-            >
-              Registrate
-            </Link>
+          <h1>Inicia sesión en tu cuenta</h1>
+          <div className="subtitle">
+            <p>¿Aún no tienes una cuenta?</p>
+              <Link className="link" to="/register">Registrate</Link>
           </div>
           <div className="flex flex-col space-y-6 mb-6 mt-6">
             <FormLogin handleLogin={handleLogin} />
