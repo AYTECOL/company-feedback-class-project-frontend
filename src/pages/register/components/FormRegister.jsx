a6349a3bb070e94ea5671000701cd61f8e232ff8
AntrugosAyte <anderson@ayte.co>
2024-05-09 20:49:55 -0500

    [FEATURE] complete styled on register form page

--- src/pages/register/components/FormRegister.jsx
+++ src/pages/register/components/FormRegister.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import "../style.css";
+import passwordIcon from "../../../assets/password.svg";
+import userIcon from "../../../assets/user.svg";
+import "./style.css";
 
 const FormRegister = ({ handleSignUp }) => {
   const {
@@ -10,24 +12,39 @@ const FormRegister = ({ handleSignUp }) => {
   } = useForm();
   return (
     <form className="form" onSubmit={handleSubmit(handleSignUp)}>
-      <div>
+      <article>
         <label>Correo Electronico:</label>
-        <input type="text" {...register("email", { required: true })} />
-        {errors.email && <span>Este campo es obligatorio</span>}
-      </div>
-      <div>
+        <div className="inputField">
+          <div className="image">
+            <img src={userIcon} alt="" />
+          </div>
+          <input type="text" {...register("email", { required: true })} />
+          {errors.email && <span>Este campo es obligatorio</span>}
+        </div>
+      </article>
+      <article>
         <label>Contraseña:</label>
+        <div className="inputField">
+          <div className="image">
+            <img src={passwordIcon} alt="" />
+          </div>
         <input type="password" {...register("password", { required: true })} />
         {errors.password && <span>Este campo es obligatorio</span>}
-      </div>
-      <div>
+        </div>
+      </article>
+      <article>
         <label>Confirmar Contraseña:</label>
-        <input
-          type="password"
-          {...register("passwordConfirmation", { required: true })}
-        />
-        {errors.passwordConfirmation && <span>Este campo es obligatorio</span>}
-      </div>
+        <div className="inputField">
+          <div className="image">
+            <img src={passwordIcon} alt="" />
+          </div>
+          <input
+            type="password"
+            {...register("passwordConfirmation", { required: true })}
+          />
+          {errors.passwordConfirmation && <span>Este campo es obligatorio</span>}
+        </div>
+      </article>
       <button className="registerButton" type="submit">
         Crear Usuario
       </button>
