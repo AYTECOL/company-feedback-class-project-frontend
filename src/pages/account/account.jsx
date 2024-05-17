2488e0537d1ae9b7511042f14b62b1d54bb2a961
AntrugosAyte <anderson@ayte.co>
2024-05-17 10:45:32 -0500

    [FEATUER] added update data for account company

--- src/pages/account/account.jsx
+++ src/pages/account/account.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import API from '../../service/API';
 import './style.css';
 
 export default function Account() {
+    const handleUpdate = async ({ companyDescription, numberEmployees, foundation, businessName, companyName }) => {
+        try {
+            await API("update", {
+                companyName: companyName,
+                businessName: businessName,
+                foundation: foundation,
+                numberEmployees: numberEmployees,
+                companyDescription: companyDescription
+            });
+        } catch (error) {
+            console.error(error);
+        }
+        };
 
+        const {
+            register,
+            handleSubmit,
+            formState: { errors },
+          } = useForm();
   return (
     <main className="account">
         <div className='headerText'>
@@ -12,7 +32,7 @@ export default function Account() {
             <button lacssName='tablinks'>Perfil</button>
         </div>
       <div className='tabcontent'>
-        <article className='sessionData'>
+        <form className='sessionData'>
             <h3>Datos de inicio de sesión</h3>
             <div className='fieldProfile'>
                 <label >Correo electrónico</label>
@@ -25,33 +45,33 @@ export default function Account() {
             <div className='updateButton'>
                 <button disabled>Actualizar</button>
             </div>
-        </article>
-        <article className='companyData'>
+        </form>
+        <form className='companyData' onSubmit={handleSubmit(handleUpdate)}>
             <h3>Datos de la compañia</h3>
             <div className='fieldProfile'>
                 <label >Nombre de la compañia</label>
-                <input type="text" />
+                <input type="text" {...register("companyName", { required: true })}/>
             </div>
             <div className='fieldProfile'>
                 <label >Razón social</label>
-                <input type="text" />
+                <input type="text" {...register("businessName", { required: true })}/>
             </div>
             <div className='fieldProfile'>
                 <label >Fecha de creación</label>
-                <input type="text" />
+                <input type="text" {...register("foundation", { required: true })}/>
             </div>
             <div className='fieldProfile'>
                 <label >Número de empleados</label>
-                <input type="text" />
+                <input type="text" {...register("numberEmployees", { required: true })}/>
             </div>
             <div className='fieldProfile'>
                 <label >Descripción de la compañia</label>
-                <input type="text" />
+                <input type="text" {...register("companyDescription", { required: true })}/>
             </div>
             <div className='updateButton'>
-                <button>Actualizar</button>
+                <button type='submit'>Actualizar</button>
             </div>
-        </article>
+        </form>
       </div>
     </main>
   )
