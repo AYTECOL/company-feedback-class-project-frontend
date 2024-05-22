bcb22d9ee3d7dd7b1af18cecfa73ab3d2feda4fe
AntrugosAyte <anderson@ayte.co>
2024-05-22 02:38:30 -0500

    [FEATURE] added survey details button and other buttons for update,edit and publish survey

---
+++ src/components/surveys/Surveys.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css'
+
+const Surveys = ({ survey }) => {
+    return (
+      <main className="survey-details">
+        <div className="container-survey">
+          <h2 className="subtitle-survey">Detalles de la Encuesta</h2>
+          <div className='details-container'>
+            <p><strong>Nombre de la encuesta:</strong> {survey.name}</p>
+            <p><strong>Fecha de creación:</strong> {survey.dateCreated}</p>
+            <p><strong>Fecha de publicación:</strong> {survey.datePublished || '-'}</p>
+            <p><strong>Estado:</strong> {survey.published ? 'Publicada' : 'No publicada'}</p>
+          </div>
+        </div>
+      </main>
+    )
+}
+
+export default Surveys
--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import APIList from '../../service/surveys/APIList';
 import "./style.css";
 import { Link } from 'react-router-dom';
+import Surveys from '../../components/surveys/Surveys.jsx';
 
 export default function Dashboard() {
   const [surveys, setSurveys] = useState([]);
+  const [selectedSurvey, setSelectedSurvey] = useState(null);
 
   const ListSurvey = async () => {
     try {
@@ -19,56 +21,42 @@ export default function Dashboard() {
     ListSurvey();
   }, []);
 
+  const handleShowSurvey = (survey) => {
+    setSelectedSurvey(survey);
+  };
+
   return (
-    <main className="dashboard">
+    <main className="dashboard-survey">
       <h1 className="title">Dashboard</h1>
       <div className="container-dashboard">
       <div className="tabs">
           <button className='tablink'>Mis Encuestas</button>
           <button className='tablink'>Resultados</button>
       </div>
-      <div className='tabcontent-survey'>
-        <table className='surveys-list'>
-          <thead>
-            <tr>
-              <th>Nombre de encuesta</th>
-              <th>Opciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {surveys.map(survey => (
-                <tr key={survey.id} className='surveys-list__row'>
-                  <td><span>{survey.name}</span></td>
-                  <td>
-                      <Link className='button-surveys' id='update' to={'run/' + survey.id}><span>Actualizar</span></Link>
-                      <Link className='button-surveys' id='edit' to={'run/' + survey.id}><span>Editar</span></Link>
-                      <Link className='button-surveys' id='publish' to={'edit/' + survey.id}><span>Publicar</span></Link>
-                  </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {/* <table className="table">
+        <div className='tabcontent-survey'>
+          <table className='surveys-list'>
             <thead>
               <tr>
-                <th>Título</th>
-                <th>Fecha de creación</th>
-                <th>Fecha de publicación</th>
-                <th>Estado</th>
+                <th>Nombre de encuesta</th>
+                <th>Opciones</th>
               </tr>
             </thead>
             <tbody>
-            {surveys.map(survey => (
-                <tr key={survey.surveyId} className='data'>
-                  <td>{survey.name}</td>
-                  <td>{survey.dateCreated }</td>
-                  <td>{survey.datePublished }</td>
-                  <td>{survey.published  ? 'Publicada' : 'No publicada'}</td>
+              {surveys.map(survey => (
+                  <tr key={survey.id} className='surveys-list__row'>
+                    <td><span>{survey.name}</span></td>
+                    <td>
+                        <button className='button-surveys' id='show' onClick={() => handleShowSurvey(survey)}>Ver</button>
+                        <Link className='button-surveys' id='update' to={'run/' + survey.id}><span>Actualizar</span></Link>
+                        <Link className='button-surveys' id='edit' to={'run/' + survey.id}><span>Editar</span></Link>
+                        <Link className='button-surveys' id='publish' to={'edit/' + survey.id}><span>Publicar</span></Link>
+                    </td>
                 </tr>
               ))}
             </tbody>
-        </table> */}
-      </div>
+          </table>
+          {selectedSurvey && <Surveys survey={selectedSurvey} />}
+        </div>
       </div>
     </main>
   );
