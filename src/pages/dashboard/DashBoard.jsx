16cb89e8b58a8f913b452429cb1ddfaa6ca349ab
AntrugosAyte <anderson@ayte.co>
2024-05-22 01:56:40 -0500

    [FEATUER] styled surveys on dashboard

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -1,39 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import APIList from '../../service/surveys/APIList';
 import "./style.css";
+import { Link } from 'react-router-dom';
 
-// Componente principal del dashboard
 export default function Dashboard() {
-  const surveys = [
-    { id: 1, title: 'Encuesta 1', description: 'Descripción de la encuesta 1' },
-    { id: 2, title: 'Encuesta 2', description: 'Descripción de la encuesta 2' },
-    { id: 3, title: 'Encuesta 3', description: 'Descripción de la encuesta 3' },
-    { id: 4, title: 'Encuesta 4', description: 'Descripción de la encuesta 4' },
-    { id: 5, title: 'Encuesta 5', description: 'Descripción de la encuesta 5' },
-  ];
+  const [surveys, setSurveys] = useState([]);
+
+  const ListSurvey = async () => {
+    try {
+      const surveys = await APIList("list");
+      setSurveys(surveys);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    ListSurvey();
+  }, []);
 
   return (
     <main className="dashboard">
       <h1 className="title">Dashboard</h1>
       <div className="container-dashboard">
-        <h2 className="subtitle">Encuestas</h2>
-        <table className="table">
+      <div className="tabs">
+          <button className='tablink'>Mis Encuestas</button>
+          <button className='tablink'>Resultados</button>
+      </div>
+      <div className='tabcontent-survey'>
+        <table className='surveys-list'>
+          <thead>
+            <tr>
+              <th>Nombre de encuesta</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {surveys.map(survey => (
+                <tr key={survey.id} className='surveys-list__row'>
+                  <td><span>{survey.name}</span></td>
+                  <td>
+                      <Link className='button-surveys' id='update' to={'run/' + survey.id}><span>Actualizar</span></Link>
+                      <Link className='button-surveys' id='edit' to={'run/' + survey.id}><span>Editar</span></Link>
+                      <Link className='button-surveys' id='publish' to={'edit/' + survey.id}><span>Publicar</span></Link>
+                  </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* <table className="table">
             <thead>
               <tr>
-                <th>ID</th>
                 <th>Título</th>
-                <th>Descripción</th>
+                <th>Fecha de creación</th>
+                <th>Fecha de publicación</th>
+                <th>Estado</th>
               </tr>
             </thead>
             <tbody>
-              {surveys.map(survey => (
-                <tr key={survey.id} className='data'>
-                  <td>{survey.id}</td>
-                  <td>{survey.title}</td>
-                  <td>{survey.description}</td>
+            {surveys.map(survey => (
+                <tr key={survey.surveyId} className='data'>
+                  <td>{survey.name}</td>
+                  <td>{survey.dateCreated }</td>
+                  <td>{survey.datePublished }</td>
+                  <td>{survey.published  ? 'Publicada' : 'No publicada'}</td>
                 </tr>
               ))}
             </tbody>
-        </table>
+        </table> */}
+      </div>
       </div>
     </main>
   );
--- src/pages/dashboard/Surveys.jsx
+++ src/pages/dashboard/Surveys.jsx
@@ -1,39 +1,67 @@
-import React, { useEffect } from 'react'
-import { create, load, remove } from '../redux/surveys'
-import { useReduxDispatch, useReduxSelector } from '../redux'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Surveys.css'
 
 const Surveys = () => {
-    const surveys = useReduxSelector(state => state.surveys.surveys)
-    const dispatch = useReduxDispatch()
+    const [surveys, setSurveys] = useState([]);
 
-    const postStatus = useReduxSelector(state => state.surveys.status)
+    const ListSurvey = async () => {
+    try {
+        const surveys = await APIList("list");
+        setSurveys(surveys);
+    } catch (error) {
+        console.error(error.message);
+    }
+    };
 
     useEffect(() => {
-      if (postStatus === 'idle') {
-        dispatch(load())
-      }
-    }, [postStatus, dispatch])    
+    ListSurvey();
+    }, []);
 
-    return (<>
-        <table className='sjs-surveys-list'>
-            {surveys.map(survey => 
-                <tr key={survey.id} className='sjs-surveys-list__row'>
-                    <td><span>{survey.name}</span></td>
-                    <td>
-                        <Link className='sjs-button' to={'run/' + survey.id}><span>Run</span></Link>
-                        <Link className='sjs-button' to={'edit/' + survey.id}><span>Edit</span></Link>
-                        <Link className='sjs-button' to={'results/' + survey.id}><span>Results</span></Link>
-                        <span className='sjs-button sjs-remove-btn' onClick={() => dispatch(remove(survey.id))}>Remove</span>
-                    </td>
+    return (
+        <main className="dashboard">
+        <h1 className="title">Dashboard</h1>
+        <div className="container-dashboard">
+          <h2 className="subtitle">Encuestas</h2>
+          <table className="table">
+              <thead>
+                <tr>
+                  <th>Título</th>
+                  <th>Fecha de creación</th>
+                  <th>Fecha de publicación</th>
+                  <th>Estado</th>
                 </tr>
-            )}
-        </table>
-        <div className='sjs-surveys-list__footer'>
-            <span className='sjs-button sjs-add-btn' title='increment' onClick={() => dispatch(create())}>Add Survey</span>                        
+              </thead>
+              <tbody>
+              {surveys.map(survey => (
+                  <tr key={survey.surveyId} className='data'>
+                    <td>{survey.name}</td>
+                    <td>{survey.dateCreated }</td>
+                    <td>{survey.datePublished }</td>
+                    <td>{survey.published  ? 'Publicada' : 'No publicada'}</td>
+                  </tr>
+                ))}
+              </tbody>
+          </table>
         </div>
-    </>)
+      </main>
+        // <table className='sjs-surveys-list'>
+        //     {surveys.map(survey => 
+        //         <tr key={survey.id} className='sjs-surveys-list__row'>
+        //             <td><span>{survey.name}</span></td>
+        //             <td>
+        //                 <Link className='sjs-button' to={'run/' + survey.id}><span>Run</span></Link>
+        //                 <Link className='sjs-button' to={'edit/' + survey.id}><span>Edit</span></Link>
+        //                 <Link className='sjs-button' to={'results/' + survey.id}><span>Results</span></Link>
+        //                 <span className='sjs-button sjs-remove-btn' onClick={() => dispatch(remove(survey.id))}>Remove</span>
+        //             </td>
+        //         </tr>
+        //     )}
+        // </table>
+        // <div className='sjs-surveys-list__footer'>
+        //     <span className='sjs-button sjs-add-btn' title='increment' onClick={() => dispatch(create())}>Add Survey</span>                        
+        // </div>
+    )
 }
 
 export default Surveys
