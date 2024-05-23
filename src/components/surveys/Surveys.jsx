e357974b29720d4f4e7b7bcb741a863f8e14f8df
AntrugosAyte <anderson@ayte.co>
2024-05-23 14:37:47 -0500

    [FEATURE] added questions on survey detail

--- src/components/surveys/Surveys.jsx
+++ src/components/surveys/Surveys.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './style.css'
 
 const Surveys = ({ survey }) => {
+  const questionsList = survey.questions;
+
     return (
       <main className="survey-details">
         <div className="container-survey">
@@ -13,6 +15,17 @@ const Surveys = ({ survey }) => {
             <p><strong>Fecha de publicación:</strong> {survey.datePublished || '-'}</p>
             <p><strong>Fecha de modificación:</strong> {survey.dateModified || '-'}</p>
             <p><strong>Estado:</strong> {survey.published ? 'Publicada' : 'No publicada'}</p>
+            <div>
+              <p><strong>Preguntas:</strong></p>
+              <ul>
+              {questionsList.map((questionItem, index) => (
+                <li key={index}>
+                  <p><strong>Pregunta:</strong> {questionItem.question}</p>
+                  <p><strong>Tipo:</strong> {questionItem.type === 'open' ? 'Abierta' : 'Cerrada'}</p>
+                </li>
+              ))}
+            </ul>
+            </div>
           </div>
         </div>
       </main>
